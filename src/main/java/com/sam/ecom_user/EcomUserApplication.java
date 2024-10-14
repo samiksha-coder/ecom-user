147b900ba0490a393fe1a849a0778997bb21aba4
Samiksha Goyal <samiksha.g17@gmail.com>
2024-10-14 15:47:21 +0530

    init

---
+++ src/main/java/com/sam/ecom_user/EcomUserApplication.java
@@ -0,0 +1,13 @@
+package com.sam.ecom_user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcomUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcomUserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sam/ecom_user/EcomUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sam.ecom_user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
